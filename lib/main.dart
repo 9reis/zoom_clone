359ed14db01553a12e6402c503f9f7c2ee92230e
Lucas Reis <lucasreis_9@hotmail.com>
2023-07-22 18:51:44 -0300

    Firebase setup Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:zoom_clone/screens/home_screen.dart';
 import 'package:zoom_clone/screens/login_screen.dart';
