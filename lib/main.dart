2f28914072c8ee882df0125e12a6736353232081
Lucas Reis <lucasreis_9@hotmail.com>
2023-07-22 19:03:01 -0300

    Persisting User State

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/resources/auth_methods.dart';
 import 'package:zoom_clone/screens/home_screen.dart';
 import 'package:zoom_clone/screens/login_screen.dart';
 import 'package:zoom_clone/utils/colors.dart';
@@ -30,7 +31,20 @@ class MyApp extends StatelessWidget {
         '/login': (context) => LoginScreen(),
         'home': (context) => HomeScreen(),
       },
-      home: LoginScreen(),
+      home: StreamBuilder(
+          stream: AuthMethods().authChanges,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (snapshot.hasData) {
+              return HomeScreen();
+            }
+            return LoginScreen();
+          }),
     );
   }
 }
